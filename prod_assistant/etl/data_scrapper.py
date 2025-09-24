7a2061bb727fe94f71b67a5908280fc87f08763d
Vivek Chandan <vivekchandan73@gmail.com>
2025-09-24 12:13:47 +0530

    logger , exception , ingestion & scrapper module created

---
+++ prod_assistant/etl/data_scrapper.py
@@ -0,0 +1,25 @@
+import csv
+import time
+import os
+import re
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+import undetected_chromedriver as uc
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+class FlipkartScraper:
+    def __init__(self, output_dir="data"):
+        self.output_dir = output_dir
+        os.makedirs(self.output_dir, exist_ok=True)
+        
+    def get_top_reviews(self, product_url, count=2):
+        pass
+    
+    def scrape_flipkart_products(self, query, max_products=1, review_count=2):
+        pass
+    
+    def save_to_csv(self, data, filename="product_reviews.csv"):
+        pass
+    
+    
